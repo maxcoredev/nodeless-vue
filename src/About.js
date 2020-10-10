e4b31d113c5d2c5c03a4c3b678140a25eeb96a0d
Максим Коротеев <Max@myhost.local>
2020-10-10 05:54:28 +0300

    first commit

--- src/About.js
+++ src/About.js
@@ -1,7 +1,11 @@
+import Button from '/Button.js';
+
 export default { template: `
 
 <h1>About</h1>
 
 <p>Some company info</p>
+
+<Button></Button>
 
 `}
---
+++ src/Button.js
@@ -0,0 +1,5 @@
+export default { template: `
+
+    <button>Button</button>
+   
+`}
